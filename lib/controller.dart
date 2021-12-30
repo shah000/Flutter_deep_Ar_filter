02d3c02c102b4813188675fc7ff33959abce9cdb
shah000 <shahjihussain42@gmail.com>
2021-12-30 13:45:09 -0800

    first commit

---
+++ lib/controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class DeepArController extends GetxController {
+  final String Androidkey =
+      '23b9c03cd8f7c20f16eb611db5db199f9c487a077edc652948861cccebad7ed1a90f78f8ed5acb14';
+}
